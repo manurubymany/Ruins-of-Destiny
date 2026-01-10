91d3da6343c3b28fcafa05e5a622eec7d48cc21e
manurubymany <119502036+manurubymany@users.noreply.github.com>
2026-01-10 20:06:02 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'ruins-cache-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './assets/logo.png',
+  // adicione aqui outros arquivos essenciais do seu CSS/JS
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
